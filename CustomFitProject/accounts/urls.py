3172c790f518efcde7dfb53ea51e06fc107bf693
likelionTeamOne <likelionteamone@gmail.com>
2024-07-27 17:10:47 +0900

    단계별 회원가입

--- CustomFitProject/accounts/urls.py
+++ CustomFitProject/accounts/urls.py
@@ -1,21 +1,10 @@
 from django.urls import path, include
-# from .views import (
-#     UserRegistrationView, UserAgeUpdateView, UserGenderUpdateView, 
-#     UserDiseaseUpdateView, UserHeightUpdateView, UserWeightUpdateView
-# )
 from .views import (
     UserRegistrationStep1View, UserRegistrationStep2View, UserRegistrationStep3View, 
     UserRegistrationStep4View, UserRegistrationStep5View, UserRegistrationStep6View
 )
 
 urlpatterns = [
-    # path('register/', UserRegistrationView.as_view(), name='register'),
-    # path('register/age/', UserAgeUpdateView.as_view(), name='register-age'),
-    # path('register/gender/', UserGenderUpdateView.as_view(), name='register-gender'),
-    # path('register/disease/', UserDiseaseUpdateView.as_view(), name='register-disease'),
-    # path('register/height/', UserHeightUpdateView.as_view(), name='register-height'),
-    # path('register/weight/', UserWeightUpdateView.as_view(), name='register-weight'),
-    
     path('register/step1/', UserRegistrationStep1View.as_view(), name='register-step1'),
     path('register/step2/', UserRegistrationStep2View.as_view(), name='register-step2'),
     path('register/step3/', UserRegistrationStep3View.as_view(), name='register-step3'),
--- CustomFitProject/accounts/views.py
+++ CustomFitProject/accounts/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from django.core.exceptions import PermissionDenied
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticated
 from .serializers import (
     UserRegistrationSerializer, UserAgeSerializer, UserGenderSerializer, 
     UserDiseaseSerializer, UserHeightSerializer, UserWeightSerializer
@@ -16,11 +15,18 @@ from .models import CustomUser
 # 1단계) 회원 정보, 약관  >> 여기 코드 추가해야함
 class UserRegistrationStep1View(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
-    permission_classes = [AllowAny]     # 미로그인 상태 가능하게
+    permission_classes = [AllowAny]     # 모든 사용자가 접근
 
-    def perform_create(self, serializer):
-        self.request.session['registration_data'] = serializer.validated_data
-        serializer.save()
+    def perform_create(self, serializer):   # 사용자가 등록될 때 호출
+        user = serializer.save()    # 새로운 사용자 객체를 저장
+
+        # 세션에 사용자 이름과 비밀번호를 저장
+        self.request.session['registration_data'] = {
+            'username': user.username,
+            'password': serializer.validated_data['password']
+        }
+        self.request.session.save()     # 세션 데이터를 저장
+
 
 # 2단계) 나이
 class UserRegistrationStep2View(generics.UpdateAPIView):
@@ -28,11 +34,20 @@ class UserRegistrationStep2View(generics.UpdateAPIView):
     permission_classes = [AllowAny]
 
     def get_object(self):
-        return CustomUser.objects.get(username=self.request.session['registration_data']['username'])
+        # 세션에서 사용자 이름을 가져와 해당 사용자 객체를 반환
+        username = self.request.session.get('registration_data', {}).get('username')
+        
+        # 세션에 사용자 이름이 없으면 PermissionDenied 예외를 발생
+        if not username:
+            raise PermissionDenied("No registration data found in session.")
+        return CustomUser.objects.get(username=username)
 
+    # 사용자가 나이를 업데이트할 때 호출
     def perform_update(self, serializer):
+        # 세션에 저장된 데이터에 새로운 나이 데이터를 추가
         self.request.session['registration_data'].update(serializer.validated_data)
-        serializer.save()
+        self.request.session.save()     # 세션 데이터를 저장
+        serializer.save()   # 사용자 객체를 저장
 
 # 3단계) 성별
 class UserRegistrationStep3View(generics.UpdateAPIView):
@@ -40,10 +55,14 @@ class UserRegistrationStep3View(generics.UpdateAPIView):
     permission_classes = [AllowAny]
 
     def get_object(self):
-        return CustomUser.objects.get(username=self.request.session['registration_data']['username'])
+        username = self.request.session.get('registration_data', {}).get('username')
+        if not username:
+            raise PermissionDenied("No registration data found in session.")
+        return CustomUser.objects.get(username=username)
 
     def perform_update(self, serializer):
         self.request.session['registration_data'].update(serializer.validated_data)
+        self.request.session.save()
         serializer.save()
 
 # 4단계) 질병
@@ -52,10 +71,14 @@ class UserRegistrationStep4View(generics.UpdateAPIView):
     permission_classes = [AllowAny]
 
     def get_object(self):
-        return CustomUser.objects.get(username=self.request.session['registration_data']['username'])
+        username = self.request.session.get('registration_data', {}).get('username')
+        if not username:
+            raise PermissionDenied("No registration data found in session.")
+        return CustomUser.objects.get(username=username)
 
     def perform_update(self, serializer):
         self.request.session['registration_data'].update(serializer.validated_data)
+        self.request.session.save()
         serializer.save()
 
 # 5단계) 키
@@ -64,29 +87,49 @@ class UserRegistrationStep5View(generics.UpdateAPIView):
     permission_classes = [AllowAny]
 
     def get_object(self):
-        return CustomUser.objects.get(username=self.request.session['registration_data']['username'])
+        username = self.request.session.get('registration_data', {}).get('username')
+        if not username:
+            raise PermissionDenied("No registration data found in session.")
+        return CustomUser.objects.get(username=username)
 
     def perform_update(self, serializer):
         self.request.session['registration_data'].update(serializer.validated_data)
+        self.request.session.save()
         serializer.save()
 
-
-# 6단계) 몸무게 >> 여기서 회원이 저장되도록 함
+# 6단계) 몸무게
 class UserRegistrationStep6View(generics.UpdateAPIView):
     serializer_class = UserWeightSerializer
     permission_classes = [AllowAny]
 
+    # 세션에서 사용자 이름을 가져와 해당 사용자 객체를 반환
     def get_object(self):
-        return CustomUser.objects.get(username=self.request.session['registration_data']['username'])
+        username = self.request.session.get('registration_data', {}).get('username')
+        if not username:
+            raise PermissionDenied("No registration data found in session.")
+        return CustomUser.objects.get(username=username)
 
+    # 사용자가 몸무게를 업데이트할 때 호출
     def perform_update(self, serializer):
-        registration_data = self.request.session.pop('registration_data')
+        # 세션에서 등록 데이터를 가져오기
+        registration_data = self.request.session.get('registration_data', {})
+        #  세션에 저장된 데이터에 새로운 몸무게 데이터를 추가
         registration_data.update(serializer.validated_data)
-        user = CustomUser.objects.get(username=registration_data['username'])
-        user.age = registration_data['age']
-        user.gender = registration_data['gender']
-        user.disease = registration_data['disease']
-        user.height = registration_data['height']
-        user.weight = registration_data['weight']
-        user.set_password(registration_data['password'])
-        user.save()
+
+        self.request.session.save() # 세션 데이터를 저장
+
+        # 사용자 객체를 가져와 세션 데이터를 통해 모든 필드를 업데이트
+        user = CustomUser.objects.get(username=registration_data.get('username'))
+        user.age = registration_data.get('age')
+        user.gender = registration_data.get('gender')
+        user.disease = registration_data.get('disease')
+        user.height = registration_data.get('height')
+        user.weight = registration_data.get('weight')
+        user.set_password(registration_data.get('password'))    # 비밀번호를 설정
+        
+        user.save()     # 사용자 객체를 저장
+
+        # 세션에서 등록 데이터를 삭제
+        self.request.session.pop('registration_data', None)
+
+        return serializer.data
