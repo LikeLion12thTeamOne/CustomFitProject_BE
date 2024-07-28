61b9718f94ededd98f491fcda92968093db90a62
likelionTeamOne <likelionteamone@gmail.com>
2024-07-28 21:19:57 +0900

    데이터베이스 추가

---
+++ CustomFitProject/myPage/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views import (
+    RecommendedProductListView, RecommendedProductEditView, 
+    RecommendedProductDetailView
+)
+from .views import UserAgeUpdateView, UserDiseaseUpdateView, UserHeightUpdateView, UserWeightUpdateView
+from .views import NoticeListView, NoticeDetailView
+
+urlpatterns = [
+    # 추천상품 목록 보기
+    path('recommended-products/', RecommendedProductListView.as_view(), name='recommended-product-list'),
+    # 추천상품 목록 중 추천상품 리뷰 작성하기 및 수정하기
+    path('recommended-products/<int:pk>/edit/', RecommendedProductEditView.as_view(), name='recommended-product-create-update'),
+    # 추천상품 목록 중 추천상품 리뷰 자세히 보기
+    path('recommended-products/<int:pk>/', RecommendedProductDetailView.as_view(), name='recommended-product-detail'),
+
+    #키워드 변경 
+    path('update/age/', UserAgeUpdateView.as_view(), name='update-age'), #age
+    path('update/disease/', UserDiseaseUpdateView.as_view(), name='update-disease'), #disease
+    path('update/height/', UserHeightUpdateView.as_view(), name='update-height'), #height
+    path('update/weight/', UserWeightUpdateView.as_view(), name='update-weight'), #weight
+
+    path('notices/', NoticeListView.as_view(), name='notice-list'), #전체 공지조회
+    path('notices/<int:pk>/', NoticeDetailView.as_view(), name='notice-detail'), #개별 공지확인
+]
