d199497cfe92a43058504c6bf4ac9dbc92b396c4
gayeoneee <164208099+gayeoneee@users.noreply.github.com>
2024-07-29 13:43:04 +0900

    Update settings.py

--- CustomFitProject/CustomFitProject/settings.py
+++ CustomFitProject/CustomFitProject/settings.py
@@ -20,7 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eos4gmk87&u2&!b-qrboz4$cyn3iz9cvm5u($kf@95+&af^(z2'
+secret_file = os.path.join(BASE_DIR, 'secret_key.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    return secrets[setting]
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
